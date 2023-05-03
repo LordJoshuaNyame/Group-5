9fc18ef8b96ac09e7942b7a30426111adf7cc2af
LordJoshuaNyame <nyamel0829@students.bowiestate.edu>
2023-05-03 18:16:21 -0400

    Project1-Part 1.py

---
+++ Project1-Part 1.py
@@ -0,0 +1,11 @@
+#Function to calculate BMI
+def calculate_bmi(weight, height):
+    bmi=weight / (height**2)
+    retun bmi
+
+#List of individuals data
+people_data=[
+    {"name": "Josh", "Weight": 69, "height": 1.9},
+    {"name": "Chiemela", "Weight": 55, "height": 1.5},
+    {"name": "Peter", "Weight": 95, "height": 1.1},
+]
