3ef2a131f3c698492a26dd021038e39a2a2ef145
Chi Emela <KALUCHIEMELA97@GMAIL.COM>
2023-05-15 16:55:29 -0400

    Project

---
+++ Part 1.py
@@ -0,0 +1,34 @@
+
+#derive function to calculate BMI
+def calculate_bmi(weight, height):
+    bmi = weight / (height ** 2)
+    return bmi
+
+#loop to iterate through individuals' data
+Students = int(input("Enter the number of students: "))
+for i in range(3):
+    print(f"Students {i+1}:")
+    weight = float(input("Enter weight in kg:"))
+    height = float(input("Enter weight in m:"))
+    bmi = calculate_bmi(weight, height)
+
+#if/elif conditions to determine BMI category
+if bmi <18.5:
+    category = "Underweight"
+elif 18.5 <= bmi < 25:
+    category = "Normal weight"
+elif 25 <= bmi <30:
+    category = "Overweight"
+elif bmi >= 30:
+    category = "Obese"
+else:
+    category = "Invalid BMI"
+
+print(f"BMI: {bmi: .2f}), Category: {category}")
+
+#determine if given BMI number is even or od
+bmi_num = float(input("Enter a BMI number: "))
+if bmi_num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")+
